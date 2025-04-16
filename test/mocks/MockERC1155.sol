204ce166b1f9b344a5a531e61c3edc38d037cf8e
Michalis Kargakis <kargakis@protonmail.com>
2025-04-16 16:58:14 +0200

    Increase test coverage

--- test/mocks/MockERC1155.sol
+++ test/mocks/MockERC1155.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.20;
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract MockERC1155 is ERC1155 {
-    constructor() ERC1155("https://api.example.com/tokens/{id}") {}
+    constructor() ERC1155("") {}
 
-    function mint(address to, uint256 id, uint256 amount) external {
+    function mint(address to, uint256 id, uint256 amount) public {
         _mint(to, id, amount, "");
     }
 
-    function mintBatch(address to, uint256[] calldata ids, uint256[] calldata amounts) external {
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts) public {
         _mintBatch(to, ids, amounts, "");
     }
 }
